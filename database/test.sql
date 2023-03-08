889c167f40b742551033ae8aafbc412ce728fe43
Olivier VINOT <ecrire.olivier.vinot@gmail.com>
2023-03-08 17:03:45 +0100

    add comments

---
+++ database/test.sql
@@ -0,0 +1,9 @@
+INSERT INTO "comment"("content","activity_note","user_id","activity_id")
+VALUES
+
+-- Commentaires pour l'activité "Escalade en falaise"
+('Superbe sortie, la falaise était magnifique !', 3, 5, 1),
+('Belle journée, le niveau était bien adapté pour moi.', 2, 9, 1),
+('J''ai beaucoup appris avec le guide, je recommande.', 3, 8, 1),
+('Une sortie mémorable, merci à toute l''équipe !', 3, 4, 1),
+('Pas facile mais tellement satisfaisant, je suis fier de moi !', 2, 6, 1);
