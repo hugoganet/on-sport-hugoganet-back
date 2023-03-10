652c24fa4b327f3ccffdfd63647e16980eb0ff42
Olivier VINOT <ecrire.olivier.vinot@gmail.com>
2023-03-10 08:19:34 +0100

    Doc: routers/index.js

--- app/routers/index.js
+++ app/routers/index.js
@@ -1,7 +1,8 @@
+// On importe le module "express" et on crée un objet routeur
 import express from 'express';
-
 const router = express.Router();
 
+// On importe les sous-routeurs pour chaque entité de l'application
 import { authRouter } from './authRouter.js';
 //import { controlToken } from '../middlewares/jwt.js';
 import { userRouter } from './userRouter.js';
@@ -10,6 +11,7 @@ import { commentRouter } from './commentRouter.js';
 import { locationRouter } from './locationRouter.js';
 import { mainRouter } from './mainRouter.js';
 
+// On associe chaque sous-routeur à une URL de l'application
 router.use('/api/auth', authRouter);
 //router.use(controlToken.validateToken);
 router.use('/api/user', userRouter);
@@ -19,4 +21,5 @@ router.use('/api/location', locationRouter);
 router.use('/api', mainRouter);
 router.use('/', mainRouter);
 
+// On exporte l'objet routeur pour qu'il puisse être utilisé par l'application
 export { router };
