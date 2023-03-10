fad5acd3ed868057e2143a78a448f33a1c020a17
Olivier VINOT <ecrire.olivier.vinot@gmail.com>
2023-03-10 08:43:40 +0100

    Doc: routers/mainnRouter.js

--- app/routers/mainRouter.js
+++ app/routers/mainRouter.js
@@ -1,6 +1,8 @@
+// On importe le module "express" et on crée un objet routeur
 import express from 'express';
 const mainRouter = express.Router();
 
+// On définit les routes de ce routeur pour la route principale '/'
 mainRouter
   .route('/')
   .get((req, res, next) => {
@@ -13,4 +15,5 @@ mainRouter
     res.send('Other requests called');
   });
 
+// On exporte l'objet routeur pour qu'il puisse être utilisé par l'application
 export { mainRouter };
