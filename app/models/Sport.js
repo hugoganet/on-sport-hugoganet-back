d842d9eb7274645b707fceaf31c43729ec8d71f4
Olivier VINOT <ecrire.olivier.vinot@gmail.com>
2023-03-10 09:55:18 +0100

    Doc: routers/models/User_Has_Sport.js

--- app/models/Sport.js
+++ app/models/Sport.js
@@ -3,6 +3,8 @@ import { sequelize } from '../dataSource/onSportSource.js';
 
 export class Sport extends Model {}
 
+// Définition de la classe Sport qui étend la classe Model
+// pour interagir avec la table "sport" dans la base de données
 Sport.init(
   {
     name: {
@@ -11,11 +13,13 @@ Sport.init(
     },
   },
   {
-    underscored: true,
-    tableName: 'sport',
-    sequelize,
-    modelName: 'Sport',
-    createdAt: false,
-    updatedAt: false,
+    underscored: true, // Utilise des noms de colonne en minuscules avec des underscores
+    tableName: 'sport', // Nom de la table dans la base de données
+    sequelize, // Objet sequelize lié à la définition de la table
+    modelName: 'Sport', // Nom de la classe Model associée à la table
+    createdAt: false, // Pas de colonne de date de création
+    updatedAt: false, // Pas de colonne de date de mise à jour
   },
 );
+
+// La classe Sport sera utilisée pour interagir avec les enregistrements de la table "sport" dans la base de données en utilisant l'objet sequelize.
--- app/models/User_has_Sport.js
+++ app/models/User_has_Sport.js
@@ -1,25 +1,31 @@
+// Importation des classes Model et DataTypes à partir du module sequelize, ainsi que l'objet sequelize de '../dataSource/onSportSource.js'
 import { Model, DataTypes } from 'sequelize';
 import { sequelize } from '../dataSource/onSportSource.js';
 
+// Création de la classe User_has_Sport qui étend la classe Model
 export class User_has_Sport extends Model {}
 
+// Utilisation de la méthode init pour définir la table "user_has_sport" dans la base de données
 User_has_Sport.init(
   {
+    // Définition de la colonne user_id qui est de type INTEGER et peut être nulle
     user_id: {
       type: DataTypes.INTEGER,
       allowNull: true,
     },
+    // Définition de la colonne sport_id qui est de type INTEGER et ne peut pas être nulle
     sport_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
     },
   },
   {
-    underscored: true,
-    tableName: 'user_has_sport',
-    sequelize,
-    modelName: 'User_has_Sport',
-    createdAt: false,
-    updatedAt: false,
+    // Spécification d'options pour la définition de la table
+    underscored: true, // Utilisation de noms de colonnes en minuscules avec des underscores plutôt qu'en PascalCase
+    tableName: 'user_has_sport', // Nom de la table dans la base de données
+    sequelize, // Lien vers l'objet sequelize pour cette définition de table
+    modelName: 'User_has_Sport', // Nom de la classe Model pour cette définition de table
+    createdAt: false, // Ne pas ajouter de colonne de date de création
+    updatedAt: false, // Ne pas ajouter de colonne de date de mise à jour
   },
 );
