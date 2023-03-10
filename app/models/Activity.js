c859e71ad6e24c441d173fbac71d54a9cd96d668
Olivier VINOT <ecrire.olivier.vinot@gmail.com>
2023-03-10 09:30:17 +0100

    Doc: routers/models/Activity.js

--- app/models/Activity.js
+++ app/models/Activity.js
@@ -1,11 +1,14 @@
+// Importation des modules nécessaires
 import { Model, DataTypes } from 'sequelize';
 import { sequelize } from '../dataSource/onSportSource.js';
 import { Sport } from './Sport.js';
 import { Location } from './Location.js';
 import { User } from './User.js';
 
+// Création de la classe Activity qui étend la classe Model de Sequelize
 export class Activity extends Model {}
 
+// Initialisation de la structure de la table Activity avec les types de données et les contraintes
 Activity.init(
   {
     title: {
@@ -41,16 +44,18 @@ Activity.init(
       allowNull: true,
     },
   },
+  // Options de configuration de la table
   {
     underscored: true,
-    tableName: 'activity',
-    sequelize,
-    modelName: 'Activity',
-    createdAt: false,
-    updatedAt: false,
+    tableName: 'activity', // Nom de la table dans la base de données
+    sequelize, // Instance de Sequelize pour la connexion à la base de données
+    modelName: 'Activity', // Nom de la classe modèle
+    createdAt: false, // Désactive la création de la colonne createdAt dans la table
+    updatedAt: false, // Désactive la création de la colonne updatedAt dans la table
   },
 );
 
+// Association entre les tables Activity, Sport, Location et User avec les clés étrangères
 Activity.belongsTo(Sport, { foreignKey: 'sport_id' });
 Activity.belongsTo(Location, { foreignKey: 'location_id' });
 Activity.belongsTo(User, { foreignKey: 'user_id' });
