8e508cf2e3787cc9d2bcb045e4d04564415b2718
Olivier VINOT <ecrire.olivier.vinot@gmail.com>
2023-03-10 11:22:29 +0100

    Doc: routers/middlewares/uploadPhoto.js

--- app/middlewares/uploadPhoto.js
+++ app/middlewares/uploadPhoto.js
@@ -1,6 +1,10 @@
+// Importation de la bibliothèque Multer pour la gestion des fichiers
 import multer from 'multer';
+
+// Définition de la taille maximale autorisée pour les fichiers (20 Mo)
 const maxSize = 20 * 1024 * 1024;
 
+// Configuration de l'emplacement de stockage des fichiers uploadés et du nom de fichier
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, './app/photos');
@@ -11,7 +15,8 @@ const storage = multer.diskStorage({
   },
 });
 
+// Exportation de la fonction d'upload de fichiers, qui utilise Multer
 export const uploadFile = multer({
-  storage: storage,
-  limits: { fileSize: maxSize },
-}).array('photo');
+  storage: storage, // Configuration de l'emplacement de stockage des fichiers uploadés
+  limits: { fileSize: maxSize }, // Configuration de la taille maximale autorisée pour les fichiers
+}).array('photo'); // Configuration de la propriété name de l'input file HTML
