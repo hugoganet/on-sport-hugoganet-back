8c12119a07f9980efd70d04e88382ebca9cf7199
Olivier VINOT <ecrire.olivier.vinot@gmail.com>
2023-03-10 09:39:18 +0100

    Doc: routers/models/Comment.js

--- app/models/Comment.js
+++ app/models/Comment.js
@@ -3,31 +3,32 @@ import { sequelize } from '../dataSource/onSportSource.js';
 
 export class Comment extends Model {}
 
+// Définition des champs de la table "Comment"
 Comment.init(
   {
     content: {
-      type: DataTypes.STRING,
-      allowNull: true,
+      type: DataTypes.STRING, // Type de données : chaîne de caractères
+      allowNull: true, // La valeur peut être nulle
     },
     activity_note: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
+      type: DataTypes.INTEGER, // Type de données : entier
+      allowNull: true, // La valeur peut être nulle
     },
     user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
+      type: DataTypes.INTEGER, // Type de données : entier
+      allowNull: false, // La valeur ne peut pas être nulle
     },
     activity_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
+      type: DataTypes.INTEGER, // Type de données : entier
+      allowNull: false, // La valeur ne peut pas être nulle
     },
   },
   {
-    underscored: true,
-    tableName: 'comment',
-    sequelize,
-    modelName: 'Comment',
-    createdAt: false,
-    updatedAt: false,
+    underscored: true, // Utilisation de la convention de nommage snake_case
+    tableName: 'comment', // Nom de la table dans la base de données
+    sequelize, // Connexion à la base de données
+    modelName: 'Comment', // Nom du modèle
+    createdAt: false, // Pas de champ "created_at"
+    updatedAt: false, // Pas de champ "updated_at"
   },
 );
