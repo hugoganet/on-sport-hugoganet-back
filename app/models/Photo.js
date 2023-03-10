9ab25b344ca46a8eb5c51fbb36cce5e2f5d4fcf8
Olivier VINOT <ecrire.olivier.vinot@gmail.com>
2023-03-10 09:47:26 +0100

    Doc: routers/models/Photo.js

--- app/models/Photo.js
+++ app/models/Photo.js
@@ -1,8 +1,10 @@
 import { Model, DataTypes } from 'sequelize';
 import { sequelize } from '../dataSource/onSportSource.js';
 
+// Définition du modèle Photo qui étend la classe Model de Sequelize
 export class Photo extends Model {}
 
+// Initialisation du modèle Photo avec les attributs de la table correspondante
 Photo.init(
   {
     name: {
@@ -19,11 +21,11 @@ Photo.init(
     },
   },
   {
-    underscored: true,
-    tableName: 'photo',
-    sequelize,
-    modelName: 'Photo',
-    createdAt: false,
-    updatedAt: false,
+    underscored: true, // Utilise la notation underscored pour les noms des champs
+    tableName: 'photo', // Nom de la table correspondante dans la base de données
+    sequelize, // Instance de Sequelize
+    modelName: 'Photo', // Nom du modèle
+    createdAt: false, // Désactive la gestion automatique du champ createdAt
+    updatedAt: false, // Désactive la gestion automatique du champ updatedAt
   },
 );
