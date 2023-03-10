49e67d26b5f270a2b2fba27364f39c235da1d41a
Olivier VINOT <ecrire.olivier.vinot@gmail.com>
2023-03-10 08:46:09 +0100

    Doc: routers/userRouter.js

--- app/routers/userRouter.js
+++ app/routers/userRouter.js
@@ -5,16 +5,20 @@ import { userController } from '../controllers/userController.js';
 import { uploadFile } from '../middlewares/uploadPhoto.js';
 import { controlUnique } from '../middlewares/controlData.js';
 import { controlSyntaxMiddleware } from '../middlewares/controlSyntaxMiddleware.js';
+
+// Routes relatives aux profils utilisateur
 userRouter
   .route('/profil/:id')
-  .get(controlSyntaxMiddleware.syntaxIdControl, userController.getProfil)
+  .get(controlSyntaxMiddleware.syntaxIdControl, userController.getProfil) // Route pour obtenir un profil d'utilisateur
   .patch(
-    controlSyntaxMiddleware.syntaxIdControl,
-    uploadFile,
-    controlUnique.uniqueProfil,
-    userController.modifyProfil,
+    // Route pour modifier un profil d'utilisateur
+    controlSyntaxMiddleware.syntaxIdControl, // Vérification de la syntaxe de l'ID utilisateur
+    uploadFile, // Middleware pour gérer l'upload d'une photo de profil
+    controlUnique.uniqueProfil, // Vérification de l'unicité des informations de profil
+    userController.modifyProfil, // Contrôleur pour la modification du profil
   )
-  .delete(controlSyntaxMiddleware.syntaxIdControl, userController.deleteProfil);
+  .delete(controlSyntaxMiddleware.syntaxIdControl, userController.deleteProfil); // Route pour supprimer un profil d'utilisateur
 
-userRouter.route('/profil/:id/photo/:name').get(userController.getPhoto);
+userRouter.route('/profil/:id/photo/:name').get(userController.getPhoto); // Route pour obtenir la photo de profil d'un utilisateur
+
 export { userRouter };
