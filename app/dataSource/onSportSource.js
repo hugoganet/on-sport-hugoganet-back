c74ae8b00d51bce06faf3105f176a304f8127618
Hugo Ganet <ganethugo@gmail.com>
2023-04-25 13:43:19 +0200

    fix (.env) : change DATABASE_URL var

--- app/dataSource/onSportSource.js
+++ app/dataSource/onSportSource.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 const databaseURL = process.env.DATABASE_URL;
 
 export const sequelize = new Sequelize(databaseURL, {
-  ddialect: 'postgres',
+  dialect: 'postgres',
   protocol: 'postgres',
   logging: false,
 });
